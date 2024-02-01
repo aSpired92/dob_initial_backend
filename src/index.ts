b89d3ae1bab192648048ab81710f1a5815cd8902
Maciej Kobiela <maciejkobiela92@gmail.com>
2024-02-01 14:22:39 +0000

    added server connection

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import compression from "compression"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
 import http from "http";
+import mongoose, {Promise} from "mongoose"
+import 'dotenv/config'
 
 const app = express()
 
@@ -20,3 +22,7 @@ const server = http.createServer(app)
 server.listen(8080, () => {
     console.log("Server running on http://127.0.0.1:8080")
 })
+
+mongoose.Promise = Promise
+mongoose.connect(process.env.MONGO_URL)
+mongoose.connection.on('error', (error: Error) => console.log(error))
