82e4edbe4f9b8ad2b4d0e8ac4c2ba7ccc2ec70fb
Maciej Kobiela <maciejkobiela92@gmail.com>
2024-02-02 09:36:21 +0000

    Added product to router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import client from "./client";
+import product from "./product";
 
 const router = express.Router()
 
 export default (): express.Router => {
     client(router)
+    product(router)
     return router
 }
